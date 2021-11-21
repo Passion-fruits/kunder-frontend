4188eb4efbb1c96bddedf4a30ca9d2b1d186a773
jiwon <jidole041214@naver.com>
2021-11-22 00:25:49 +0900

    [ADD] music progress control function

--- components/common/AudioPlayer/styles.ts
+++ components/common/AudioPlayer/styles.ts
@@ -7,7 +7,9 @@ export const Wrapper = styled.div`
   bottom: 0;
   z-index: 10;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  background-color: black;
 `;
 
 export const Container = styled.div`
@@ -15,7 +17,6 @@ export const Container = styled.div`
   height: 110px;
   display: flex;
   align-items: center;
-  background-color: black;
   position: relative;
   & .music-information {
     z-index: 2;
@@ -29,4 +30,33 @@ export const ControlButtonWrapper = styled.div`
   align-items: center;
   justify-content: center;
   gap: 50px;
+`;
+
+interface rangeProps {
+  progress: number;
+}
+
+export const MusicTimeControlBar = styled.input<rangeProps>`
+  width: 100%;
+  height: 3px;
+  cursor: pointer;
+  -webkit-appearance: none;
+  background: ${(res) =>
+    `linear-gradient(to right, ${ColorMap.mainColor} 0%, ${ColorMap.mainColor}
+    ${res.progress}%, ${ColorMap.grey900} ${res.progress}%, ${ColorMap.grey900} 100%)`};
+  transition: 0.3s;
+  &:hover {
+    &::-webkit-slider-thumb {
+      opacity: 1;
+    }
+  }
+  &::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    width: 18px;
+    height: 18px;
+    border-radius: 50%;
+    background: ${ColorMap.grey000};
+    opacity: 0;
+    transition: 0.3s;
+  }
 `;
