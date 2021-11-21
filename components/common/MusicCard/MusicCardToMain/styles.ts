0a5e83e536ddeb3c80005bbe1603e73ce494c36c
jiwon <jidole041214@naver.com>
2021-11-21 15:39:30 +0900

    [CREATE] music card to main

--- components/common/MusicCard/MusicCardToMain/styles.ts
+++ components/common/MusicCard/MusicCardToMain/styles.ts
@@ -1,2 +1,83 @@
 import styled from "@emotion/styled";
 import { ColorMap } from "../../../../styles/color";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 200px;
+  & .title {
+    color: ${ColorMap.grey000};
+    font-size: 17px;
+    font-weight: 400;
+    margin-top: 10px;
+    text-align: center;
+    cursor: pointer;
+    transition: 300ms;
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+  & .artist {
+    font-size: 15px;
+    font-weight: 400;
+    color: ${ColorMap.grey300};
+    margin-top: 3px;
+    text-align: center;
+    cursor: pointer;
+    transition: 300ms;
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+`;
+
+export const MusicCoverWrap = styled.div`
+  width: 200px;
+  height: 200px;
+  border-radius: 10px;
+  overflow: hidden;
+  position: relative;
+  & img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const MusicInformation = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  z-index: 2;
+  padding: 10px;
+  display: flex;
+  & .show-cnt {
+    border-radius: 3px;
+    color: ${ColorMap.grey000};
+    font-size: 11px;
+    font-weight: 400;
+    padding: 0px 7px;
+    background-color: rgb(0, 0, 0, 0.5);
+    height: 20px;
+    display: flex;
+    align-items: center;
+    gap: 3px;
+  }
+  & .play-btn {
+    width: 45px;
+    height: 45px;
+    border-radius: 50%;
+    background-color: rgb(0, 0, 0, 0.5);
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    margin: 10px;
+    transition: 0.3s;
+    & svg {
+      margin-left: 4px;
+      margin-top: 2px;
+    }
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+`;
