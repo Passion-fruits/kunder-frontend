abe7397e5e274a8d3f4c0f7a3fcbd8f907efde4f
jiwon <jidole041214@naver.com>
2021-11-21 23:36:20 +0900

    [CREATE] header

---
+++ components/common/Header/MenuTab/styles.ts
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled";
+import { ColorMap } from "../../../../styles/color";
+
+export const MenuWrapper = styled.div`
+  width: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  gap: 80px;
+`;
+
+export const Menu = styled.button`
+  font-size: 20px;
+  font-weight: bold;
+  color: ${ColorMap.grey400};
+`;
--- components/common/Header/styles.ts
+++ components/common/Header/styles.ts
@@ -1,2 +1,48 @@
 import styled from "@emotion/styled";
 import { ColorMap } from "../../../styles/color";
+
+export const Wrapper = styled.header`
+  width: 100%;
+  position: fixed;
+  z-index: 10;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Container = styled.div`
+  width: 1400px;
+  height: 85px;
+  background-color: rgb(0, 0, 0, 0.8);
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  & .logo {
+    font-size: 30px;
+    font-family: "Agency FB";
+    color: ${ColorMap.mainColor};
+    z-index: 2;
+    cursor: pointer;
+  }
+  & .profile {
+    width: 45px;
+    height: 45px;
+    border-radius: 50%;
+    cursor: pointer;
+    z-index: 2;
+  }
+`;
+
+export const MenuWrapper = styled.div`
+  width: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  gap: 80px;
+`;
+
+export const Menu = styled.button`
+  font-size: 20px;
+  font-weight: bold;
+  color: ${ColorMap.grey400};
+`;
