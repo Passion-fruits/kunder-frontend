efed87ab3bedf0a6e0407e27a901959578e996ae
jiwon <jidole041214@naver.com>
2021-11-22 00:09:51 +0900

    [CREATE] AudioPlayer

--- components/common/AudioPlayer/index.tsx
+++ components/common/AudioPlayer/index.tsx
@@ -1,10 +1,21 @@
 import { FC } from "react";
+import MusicInformation from "../MusicInformation";
+import AudioPlayerButtonTab from "./AudioPlayerButtonTab";
 import * as S from "./styles";
 
 interface Props {}
 
 const AudioPlayer: FC<Props> = () => {
-  return <></>;
+  return (
+    <S.Wrapper>
+      <S.Container>
+        <AudioPlayerButtonTab />
+        <div className="music-information">
+          <MusicInformation type="audioPlayer" />
+        </div>
+      </S.Container>
+    </S.Wrapper>
+  );
 };
 
 export default AudioPlayer;
--- components/common/AudioPlayer/styles.ts
+++ components/common/AudioPlayer/styles.ts
@@ -1,2 +1,32 @@
 import styled from "@emotion/styled";
 import { ColorMap } from "../../../styles/color";
+
+export const Wrapper = styled.div`
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+  z-index: 10;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Container = styled.div`
+  width: 1400px;
+  height: 110px;
+  display: flex;
+  align-items: center;
+  background-color: black;
+  position: relative;
+  & .music-information {
+    z-index: 2;
+  }
+`;
+
+export const ControlButtonWrapper = styled.div`
+  width: 100%;
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 50px;
+`;
