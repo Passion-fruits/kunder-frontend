2d0e0b1df31936fdd4b319cad3d343d66f7f5e0c
jiwon <jidole041214@naver.com>
2021-11-21 22:58:07 +0900

    [UPDATE] use MusicInformation component

--- components/common/MusicCard/MusicCardToPlaylist/index.tsx
+++ components/common/MusicCard/MusicCardToPlaylist/index.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import { AddPlaylistIcon, PlayIcon, HeartIcon } from "../../../../assets";
+import MusicInformation from "../MusicInformation";
 import * as S from "./styles";
 
 interface Props {}
@@ -7,14 +8,7 @@ interface Props {}
 const MusicCardToPlaylist: FC<Props> = () => {
   return (
     <S.Wrapper>
-      <S.MusicInformation>
-        <span className="index-num">1</span>
-        <img src="https://anthoncode.com/wp-content/uploads/2020/06/poster-havana-psd-camila-cabello.jpg" />
-        <div className="title-artist-wrap">
-          <h1>좋았던 기억만 그리운 마음만</h1>
-          <h3>김팔복</h3>
-        </div>
-      </S.MusicInformation>
+      <MusicInformation type="playlist" />
       <S.ControlBtnWrap>
         <button>
           <PlayIcon size={18} />
