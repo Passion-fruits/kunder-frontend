a4a47f4b753dfecf842f77224f875cbaf64a46b0
jiwon <jidole041214@naver.com>
2021-11-22 15:23:24 +0900

    [CREATE] auth page

--- components/auth/index.tsx
+++ components/auth/index.tsx
@@ -3,8 +3,21 @@ import * as S from "./styles";
 
 interface Props {}
 
-const ComponentName: FC<Props> = () => {
-  return <></>;
+const Auth: FC<Props> = () => {
+  return (
+    <S.Wrapper>
+      <div className="line" />
+      <h1 className="title">
+        한국 #1 인디음악 서비스 <br />
+        쿤더
+      </h1>
+      <p className="description">로그인 후 더 많은 서비스를 즐겨보세요</p>
+      <S.ButtonWrap>
+        <button>로그인/가입</button>
+        <button>서비스 소개</button>
+      </S.ButtonWrap>
+    </S.Wrapper>
+  );
 };
 
-export default ComponentName;
+export default Auth;
--- components/auth/styles.ts
+++ components/auth/styles.ts
@@ -1,2 +1,47 @@
 import styled from "@emotion/styled";
 import { ColorMap } from "../../styles/color";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  & .line {
+    width: 90px;
+    height: 3px;
+    background-color: ${ColorMap.mainColor};
+  }
+  & .title {
+    font-size: 40px;
+    font-weight: bold;
+    color: ${ColorMap.grey000};
+    margin-top: 5px;
+    line-height: 60px;
+  }
+  & .description {
+    font-size: 20px;
+    font-weight: 400;
+    color: ${ColorMap.grey200};
+    margin-top: 20px;
+  }
+`;
+
+export const ButtonWrap = styled.div`
+  display: flex;
+  gap: 15px;
+  margin-top: 30px;
+  & button {
+    width: 240px;
+    height: 40px;
+    font-size: 17px;
+    color: ${ColorMap.grey000};
+    border-radius: 3px;
+    padding-bottom: 5px;
+    &:first-of-type {
+      background-color: ${ColorMap.mainColor};
+      font-weight: bold;
+    }
+    &:last-of-type {
+      border: 1px solid ${ColorMap.grey000};
+      font-weight: 400;
+    }
+  }
+`;
