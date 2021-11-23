8a8168f53e2adbbd9bf37a4df11d0629e3a6f82f
jiwon <jidole041214@naver.com>
2021-11-23 16:04:08 +0900

    [ADD] auth bottom card list

--- components/auth/styles.ts
+++ components/auth/styles.ts
@@ -28,7 +28,7 @@ export const ButtonWrap = styled.div`
   display: flex;
   gap: 15px;
   margin-top: 30px;
-  margin-bottom: 100px;
+  margin-bottom: 50px;
   & button {
     width: 240px;
     height: 40px;
@@ -43,6 +43,78 @@ export const ButtonWrap = styled.div`
     &:last-of-type {
       border: 1px solid ${ColorMap.grey000};
       font-weight: 400;
+    }
+  }
+`;
+
+export const FlexWrap = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+export const ShowMusicWrapper = styled.div<{ index }>`
+  width: 280px;
+  height: 350px;
+  display: flex;
+  align-items: center;
+  gap: 40px;
+  transform: ${({ index }) => `translateX(-${230 * index + 40 * index}px)`};
+  transition: 0.5s;
+`;
+
+export const CardWrap = styled.div<{ currentShow: boolean }>`
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  opacity: ${(res) => (res.currentShow ? 1 : 0.5)};
+  width: ${(res) => (res.currentShow ? "280px" : "230px")};
+  & button {
+    opacity: ${(res) => (res.currentShow ? 1 : 0)};
+  }
+  & .music-title {
+    font-size: ${(res) => (res.currentShow ? "20px" : "18px")};
+    margin-top: 10px;
+    color: ${ColorMap.grey000};
+    text-align: center;
+  }
+  & .artist {
+    font-size: ${(res) => (res.currentShow ? "16px" : "14px")};
+    font-weight: 400;
+    color: ${ColorMap.grey300};
+    margin-top: 2px;
+    text-align: center;
+  }
+`;
+
+export const CoverWrap = styled.div<{ currentShow: boolean }>`
+  transition: 0.5s;
+  width: ${(res) => (res.currentShow ? "280px" : "230px")};
+  height: ${(res) => (res.currentShow ? "280px" : "230px")};
+  border-radius: 10px;
+  overflow: hidden;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  & img {
+    width: 100%;
+    height: 100%;
+  }
+  & button {
+    width: 65px;
+    height: 65px;
+    border-radius: 50%;
+    border: 2px solid ${ColorMap.grey000};
+    background-color: rgb(0, 0, 0, 0.5);
+    position: absolute;
+    & svg {
+      margin-left: 8px;
+      margin-top: 2px;
+    }
+    &:hover {
+      width: 70px;
+      height: 70px;
     }
   }
 `;
