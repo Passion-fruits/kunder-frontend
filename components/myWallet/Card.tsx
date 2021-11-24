ba143916e7a8b785bca1569baf9ae36bd26ca1cd
jiwon <jidole041214@naver.com>
2021-11-24 09:23:26 +0900

    [FIX] style

--- components/myWallet/Card.tsx
+++ components/myWallet/Card.tsx
@@ -1,20 +1,24 @@
-import {FC} from "react";
+import { FC } from "react";
 import * as S from "./styles";
 
 interface CoinProps {
-    coinNum: number;
-    cost: number;
-    coin: string;
+  coinNum: number;
+  cost: number;
+  coin: string;
 }
 
-const Card: FC<CoinProps> = ({coin = "별풍선", coinNum, cost}) => {
-    return(
-        <S.Container>
-            <S.Coin>{coin} <span className="gHighlight">{coinNum}</span>개</S.Coin>
-            <S.Cost> <span className="costSpan">가격</span> <p className="won">{cost}원</p></S.Cost>
-            <S.Purchase>구매하기</S.Purchase>
-        </S.Container>
-    )
-}
+const Card: FC<CoinProps> = ({ coin, coinNum, cost }) => {
+  return (
+    <S.CardWrapper>
+      <S.Coin>
+        {coin} <span className="gHighlight">{coinNum}</span>개
+      </S.Coin>
+      <S.Cost>
+        <span className="costSpan">가격</span> <p className="won">{cost}원</p>
+      </S.Cost>
+      <S.Purchase>구매하기</S.Purchase>
+    </S.CardWrapper>
+  );
+};
 
 export default Card;
